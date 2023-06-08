8cf76c4ca3b343f6e125f8788816104c846ed5e8
Tasneem Nur <tasneemnurtashnia@gmail.com>
2023-06-08 22:20:17 +0600

    make some of the instructor from a admin dashboard and from manage user route.

--- src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,31 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import Swal from "sweetalert2";
 
 const ManageUsers = () => {
+  const [disable, setDisable] = useState(false);
   const { data: users = [], refetch } = useQuery(["users"], async () => {
     const res = await fetch("http://localhost:5000/users");
     return res.json();
   });
-  const handleMakeInstructor = user => {
+  const handleMakeInstructor = (user) => {
     fetch(`http://localhost:5000/users/instructor/${user._id}`, {
-            method: 'PATCH'
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data.modifiedCount){
-                refetch();
-                Swal.fire({
-                    position: 'center',
-                    icon: 'success',
-                    title: `${user.name} is an admin now.`,
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-            }
-        })
-
-  }
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${user.name} is an instructor now.`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+        setDisable(true);
+      });
+  };
   return (
     <div>
       <div className="overflow-x-auto">
@@ -41,16 +42,36 @@ const ManageUsers = () => {
             </tr>
           </thead>
           <tbody>
-            {
-                users.map((user, index) => <tr key={user._id}>
+            {users.map((user, index) => (
+              <tr key={user._id}>
                 <th>{index + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td>{user.role === 'admin' ? 'admin' : user.role === 'instructor' ? 'instructor' : 'student' }</td>
-                <td><button onClick={() => handleMakeInstructor(user)} className="badge badge-outline me-3">Instructor</button><button className="badge badge-outline">Admin</button></td>
-              </tr>)
-            } 
-            
+                <td>
+                  {user.role === "admin"
+                    ? "admin"
+                    : user.role === "instructor"
+                    ? "instructor"
+                    : "student"}
+                </td>
+                <td className="flex">
+                  <button
+                    disabled={user.role === "instructor"}
+                    onClick={() => handleMakeInstructor(user)}
+                    className="btn btn-xs rounded-md me-2 bg-orange-600 text-white"
+                  >
+                    Instructor
+                  </button>
+                  <button
+                    disabled={user.role === "admin"}
+                    onClick={() => handleMakeAdmin(user)}
+                    className="btn btn-xs rounded-md bg-orange-600 text-white"
+                  >
+                    Admin
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
